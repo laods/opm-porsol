96b0e80d12bd22ec8178eaa60df9bbb82aa27603
Lars Vingli Odsæter <odsater@stud.ntnu.no>
2012-09-19 16:21:36 +0200

    Further work on mortar test New code test new class MortarHelper

--- examples/mortar_mimetic_periodic_test_larso.cpp
+++ examples/mortar_mimetic_periodic_test_larso.cpp
@@ -22,11 +22,22 @@
 #include <dune/porsol/mimetic/MimeticIPEvaluator.hpp>
 #include <dune/porsol/mimetic/IncompFlowSolverHybrid.hpp>
 
+#include <dune/porsol/mortar/mortar.hpp>
+
 using namespace Dune;
 using namespace std;
 
 int main(int varnum, char** vararg)
 {
+
+  typedef Dune::GridInterfaceEuler<CpGrid>                       GI;
+  typedef GI  ::CellIterator                                     CI;
+  typedef CI  ::FaceIterator                                     FI;
+  typedef Dune::BasicBoundaryConditions<true, false>             BCs;
+  typedef Dune::ReservoirPropertyCapillary<3>                    RI;
+  typedef Dune::IncompFlowSolverHybrid<GI, RI, BCs, Dune::MimeticIPEvaluator> FlowSolver;
+
+  // Check input ---------------------------------------------------------------------------------------
 
   if (varnum != 2) { //Wrong nr of input param
     cerr << "Wrong nr of input parameters. Only eclipse grid file name is allowed." << endl;
@@ -41,6 +52,10 @@ int main(int varnum, char** vararg)
   }
   eclipsefile.close();
   
+  // Parameters
+  double ztol = 1e-8;
+  const int dim = 3;  
+  
   cout << "Parsing Eclipse file: " << ECLIPSEFILENAME << "..." << endl;
   Opm::EclipseGridParser eclParser(ECLIPSEFILENAME, true);
 
@@ -52,6 +67,31 @@ int main(int varnum, char** vararg)
     cerr << "Error: Did not find PERMX and PORO in Eclipse file " << ECLIPSEFILENAME << endl;  
     exit(1);  
   }
+
+  CpGrid grid;
+  grid.readEclipseFormat(ECLIPSEFILENAME, ztol, false);
+  int numCells = grid.size(0);
+
+  ReservoirPropertyCapillary<dim> rockParams;
+  rockParams.init(eclParser, grid.globalCell());
   
+  if (numCells < 10) {
+    cout << "PORO and PERMX:" << endl;
+    for (int c = 0; c < numCells; ++c) {
+      cout << "Cell " << c << ":\n" << rockParams.porosity(c) << "\n" << rockParams.permeability(c) << endl;
+    }
+  }
+
+  MortarHelper<CpGrid, dim> mortar;
+  mortar.findMinMax(grid);
+  mortar.find_n(grid);
+  cout << "min = " << mortar.min()[0] << " " << mortar.min()[1] << " " << mortar.min()[2] << endl;
+  cout << "max = " << mortar.max()[0] << " " << mortar.max()[1] << " " << mortar.max()[2] << endl;
+  cout << "n   = " << mortar.n1() << " " << mortar.n2() << endl;
+
+  FlowSolver solver;
+  // solver.init(...)
+  // Må gjøre noe med template BCs til FlowSolver!
+
   return 0;
 }
