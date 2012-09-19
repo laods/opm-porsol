1b482c6ad65f294b2fe81564862e9c8c28acf7c1
Lars Vingli Odsæter <odsater@stud.ntnu.no>
2012-09-19 13:24:06 +0200

    Startup implementing body in mortar_mimetic_periodic_test_larso.cpp

--- examples/mortar_mimetic_periodic_test_larso.cpp
+++ examples/mortar_mimetic_periodic_test_larso.cpp
@@ -10,6 +10,7 @@
 
 #include <opm/core/utility/Units.hpp>
 #include <opm/core/utility/parameters/ParameterGroup.hpp>
+#include <opm/core/eclipse/EclipseGridParser.hpp>
 
 #include <dune/grid/CpGrid.hpp>
 
@@ -22,9 +23,37 @@
 #include <dune/porsol/mimetic/IncompFlowSolverHybrid.hpp>
 
 using namespace Dune;
+using namespace std;
 
 int main(int argc, char** argv)
 {
 
+  if (varnum != 2) { //Wrong nr of input param
+    cerr << "Wrong nr of input parameters. Only eclipse grid file name is allowed." << endl;
+    exit(1);
+  }
+
+  const char* ECLIPSEFILENAME(vararg[1]);
+  ifstream eclipsefile(ECLIPSEFILENAME, ios::in);
+  if (eclipsefile.fail()) {
+    cerr << "Error: " << ECLIPSEFILENAME << " not found or readable." << endl;
+    exit(1);
+  }
+  eclipsefile.close();
+  
+  cout << "Parsing Eclipse file: " << ECLIPSEFILENAME << "..." << endl;
+  Opm::EclipseGridParser eclParser(ECLIPSEFILENAME, true);
+
+  if (! (eclParser.hasField("SPECGRID") && eclParser.hasField("COORD") && eclParser.hasField("ZCORN"))) {  
+    cerr << "Error: Did not find SPECGRID, COORD and ZCORN in Eclipse file " << ECLIPSEFILENAME << endl;  
+    usage();  
+    exit(1);  
+  }
+  if (! (eclParser.hasField("PERMX") && eclParser.hasField("PORO"))) {  
+    cerr << "Error: Did not find PERMX and PORO in Eclipse file " << ECLIPSEFILENAME << endl;  
+    usage();  
+    exit(1);  
+  }
+  
   return 0;
 }
